01a17a34f6e0f5261a07dd86908b5d8675dede6b
zap <zapotek6@gmail.com>
2018-03-11 13:53:04 +0100

    refactor

--- src/main/java/uk/co/labfour/cloud2/aaa_client/AAAClient.java
+++ src/main/java/uk/co/labfour/cloud2/aaa_client/AAAClient.java
@@ -1,9 +1,15 @@
 package uk.co.labfour.cloud2.aaa_client;
 
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 import uk.co.labfour.bjson.BJsonException;
 import uk.co.labfour.bjson.BJsonObject;
+import uk.co.labfour.cloud2.aaa.common.AAAConstants;
+import uk.co.labfour.cloud2.aaa.common.IAAAClient;
+import uk.co.labfour.cloud2.aaa.common.RequestInfo;
+import uk.co.labfour.cloud2.aaa.common.Utility;
+import uk.co.labfour.cloud2.microservice.ServiceError;
 import uk.co.labfour.cloud2.protocol.BaseRequest;
 import uk.co.labfour.cloud2.protocol.BaseResponse;
 import uk.co.labfour.error.BException;
@@ -12,63 +18,28 @@ import uk.co.labfour.logger.MyLoggerFactory;
 import uk.co.labfour.net.transport.IGenericTransport;
 
 public class AAAClient implements IAAAClient {
-	MyLogger log = MyLoggerFactory.getInstance();
-	Long reqId = 0L;
-	ConcurrentHashMap<String, RequestInfo> waitAuthz = new ConcurrentHashMap<String, RequestInfo>();
-	String replyTo;
-	IGenericTransport aaaTransport;
-	
-	public AAAClient(IGenericTransport aaaTransport, String replyTo) {
-		this.aaaTransport = aaaTransport;
+    MyLogger log = MyLoggerFactory.getInstance();
+	private Long reqId = 0L;
+	private final ConcurrentHashMap<String, RequestInfo> waitAuthz = new ConcurrentHashMap<>();
+	private final String replyTo;
+	private final IGenericTransport transport;
+	private final long aaaTimeoutValue = AAAConstants.DEFAULT_AAA_TIMEOUT_VALUE;
+	private final TimeUnit aaaTimeoutTimeUnit = AAAConstants.DEFAULT_AAA_TIMEOUT_TIMEUNIT;
+
+	public AAAClient(IGenericTransport transport, String replyTo) {
+		this.transport = transport;
 		this.replyTo = replyTo;
 	}
 	
-	public BaseResponse doProcess(BaseResponse response) throws BException {
-			
-			if (0 == response.getApi().compareTo(AAA_AUTHENTICATOR_API)) {
-				System.out.println("response from aaa.authenticator " + response.getPayload().toString());
-			} else if (0 == response.getApi().compareTo(AAA_AUTHORIZATOR_API)) {
-				RequestInfo requestInfo = deque(response.getReqid());
-				BaseRequest request = requestInfo.getRequest();
-				IGenericTransport transport = requestInfo.getTransport();
-				BaseResponse svcResponse = new BaseResponse(request);
-				try {
-					if (!response.containsError()) {
-						if (response.getPayload().has("authorized")) {
-							if (response.getPayload().getElementAsBoolean("authorized")) {
-								
-								// run the service
-								svcResponse = requestInfo.getService().doExec(request);		
-								
-							} else {
-								svcResponse.setError(response.getErrCode(), response.getErrDescription());
-							}
-						} else {
-							svcResponse.setError(response.getErrCode(), response.getErrDescription());
-						}
-					} else  {
-						svcResponse.setError(response.getErrCode(), response.getErrDescription());
-					}
-				} catch (BJsonException e) {
-					svcResponse.setError(response.getErrCode(), response.getErrDescription());
-				}
-				
-				// if the request is async send the response using the transport
-				// if the reuqest is sync set in the request info the response and the unblock (see CountDownLatch in RequestInfo) the wainting thread
-				if (null == requestInfo.getSyncResponse()) {
-					transport.reply(svcResponse);
-				} else {
-					requestInfo.setResponse(svcResponse);
-					requestInfo.getSyncResponse().countDown();
-				}
-				
-				System.out.println("response from aaa.authorizator " + response.getPayload().toString());
-			} else {
-				System.out.println("no API match");
-			}
-			
-			return response;
-		}
+	public BaseResponse doProcess(BaseResponse response) {
+
+		RequestInfo requestInfo = deque(response.getReqid());
+		//BaseRequest request = requestInfo.getRequest();
+		requestInfo.setResponse(response);
+		requestInfo.getSyncResponse().countDown();
+
+        return response;
+    }
 		
 	private void enque(String reqId, RequestInfo requestInfo) {
 		waitAuthz.put(reqId, requestInfo);
@@ -82,47 +53,115 @@ public class AAAClient implements IAAAClient {
 
 		synchronized (reqId) {
 			return ++reqId;
-		
 		}
+	}
+
+    private long assignUniqueIdForCorrelation(BaseRequest request, RequestInfo requestInfo) {
+        long reqId = getNextReqId();
+        request.setReqid(Long.toString(reqId));
+        requestInfo.setReqId(reqId);
+
+        return reqId;
+    }
+
+    private void send(BaseRequest request, IGenericTransport transport) throws BException {
+	    try {
+	        transport.send(request);
+        } catch (BException e) {
+            throw new BException("Transport error", ServiceError.INTERNAL_ERR);
+        }
+    }
+
+    private void sendRequest(BaseRequest request, RequestInfo requestInfo, IGenericTransport transport) throws BException {
+        long reqId = assignUniqueIdForCorrelation(request, requestInfo);
+
+        enque(Long.toString(reqId), requestInfo);
+
+        send(request, transport);
+    }
+
+    private BJsonObject createAuthzObject(RequestInfo requestInfo) throws BException {
+        BJsonObject authz = new BJsonObject();
+
+        try {
+            authz.put("targetResourceUuid", requestInfo.getTargetResourceUuid());
+            authz.put("actionOnTargetResource", requestInfo.getActionOnTargetResource());
+        } catch (BJsonException e) {
+            throw new BException("Malformed input", ServiceError.CLIENT_ERR);
+        }
+
+        return authz;
+    }
+
+    private void prepareAndAddAuthzStuff(BaseRequest request, RequestInfo requestInfo) throws BException {
+
+	    BJsonObject authz = createAuthzObject(requestInfo);
+
+	    try {
+            request.getPayload().put(AAAConstants.AUTH_CONTAINER_FLD, requestInfo.getRequest().getAuth());
+            request.getPayload().put(AAAConstants.AUTHZ_CONTAINER_FLD, authz);
+        } catch (BJsonException e) {
+            throw new BException("Malformed input", ServiceError.CLIENT_ERR);
+        }
+    }
+
+    private void addOperations(BaseRequest request, RequestInfo requestInfo) throws BException {
+        try {
+            request.getPayload().put(AAAConstants.OPERATIONS_ARRAY_CONTAINER_FLD, requestInfo.getRequest().getPayload().getElementAsBJsonArray(AAAConstants.OPERATIONS_ARRAY_CONTAINER_FLD));
+
+        } catch (BJsonException e) {
+            throw new BException("Malformed input", ServiceError.CLIENT_ERR);
+        }
+    }
+
+	public BaseResponse doAuthz(RequestInfo requestInfo, String apiKey) {
+
+        try {
+
+            BaseRequest authReq = Utility.createRequest(apiKey, AAAConstants.AAA_CONSUMER, AAAConstants.AAA_AUTHORIZATOR_API, replyTo);
+
+            prepareAndAddAuthzStuff(authReq, requestInfo);
+
+            sendRequest(authReq, requestInfo, transport);
+
+            return waitRequestCompletion(requestInfo, authReq);
+
+        } catch (BException e) {
+            return new BaseResponse(requestInfo.getRequest()).setError(e.getErrorCode(), e.getMessage());
+        }
 
 	}
-	
-	public boolean doAuthz(RequestInfo requestInfo, String apiKey) throws BException {
-		BaseRequest authReq = new BaseRequest();
-		
-		BJsonObject serviceAuth = new BJsonObject();
-		try {
-			serviceAuth.put("apikey", apiKey);
-			serviceAuth.put("mode", "apikey");
-		} catch (BJsonException e) {
-			throw new BException(e);
-		}
-		
-		authReq.setAuth(serviceAuth);
-		authReq.setApi(AAA_AUTHORIZATOR_API);
-		authReq.setConsumer(AAA_CONSUMER);
-		authReq.setReplyTo(replyTo);
-		
-		try {
-			
-			BJsonObject authz = new BJsonObject();
-			authz.put("resourceUuid", requestInfo.getResourceUuid());
-			authz.put("action", requestInfo.getAction());
-			authReq.getPayload().put(BaseRequest.kAuth, requestInfo.getRequest().getAuth());
-			authReq.getPayload().put("authz", authz);
-			
-		} catch (BJsonException e) {
-			throw new BException("error creating authz request", e);
-		}
-		
-		long reqId = getNextReqId();
-		authReq.setReqid(Long.toString(reqId));
-		requestInfo.setReqId(reqId);
-		enque(Long.toString(reqId), requestInfo);
-		aaaTransport.send(authReq);
-		return false;
+
+	public BaseResponse doCreateComplexEntity(RequestInfo requestInfo, String apiKey) {
+
+        try {
+
+            BaseRequest authReq = Utility.createRequest(apiKey, AAAConstants.AAA_CONSUMER, AAAConstants.AAA_CREATE_COMPLEX_ENTITY_API, replyTo);
+
+            prepareAndAddAuthzStuff(authReq, requestInfo);
+
+            addOperations(authReq, requestInfo);
+
+            sendRequest(authReq, requestInfo, transport);
+
+            return waitRequestCompletion(requestInfo, authReq);
+        } catch(BException e) {
+            return new BaseResponse(requestInfo.getRequest()).setError(e.getErrorCode(), e.getMessage());
+        }
+
+
 	}
-	
-	
-	
+
+	private BaseResponse waitRequestCompletion(RequestInfo requestInfo, BaseRequest authReq) throws BException {
+        requestInfo.waitCompletion(aaaTimeoutValue, aaaTimeoutTimeUnit);
+        BaseResponse response = requestInfo.getResponse();
+
+        if (null == response) {
+            response = new BaseResponse(authReq);
+            response.setError(ServiceError.INTERNAL_ERR, "AAA Service timeout");
+        }
+
+        return response;
+    }
+
 }
---
+++ src/main/java/uk/co/labfour/cloud2/aaa_client/AAAOperations.java
@@ -0,0 +1,63 @@
+package uk.co.labfour.cloud2.aaa_client;
+
+import uk.co.labfour.bjson.BJsonException;
+import uk.co.labfour.cloud2.aaa.common.*;
+import uk.co.labfour.cloud2.microservice.ServiceContext;
+import uk.co.labfour.cloud2.protocol.BaseRequest;
+import uk.co.labfour.cloud2.protocol.BaseResponse;
+import uk.co.labfour.error.BException;
+
+import static uk.co.labfour.cloud2.aaa.common.AAAConstants.*;
+
+public class AAAOperations {
+
+    public static BaseResponse createComplexEntityForUser(ServiceContext serviceContext, String name, String uid, String pwd) throws BException {
+
+        AAAEntityOperationBuilder createuserOperation = new AAAEntityOperationBuilder()
+                .setAction("1", ADD_OPE, "name", "description", "00000000-0000-0000-0000-00000000000c", "00000000-0000-0000-0000-000000000003")
+                .setAuthWithCredentials(uid, pwd)
+                .addPermission("00000000-0000-0000-0000-000000000001", PERMISSION_ACTION__CREATE_ENTITY, PERMISSION_ALLOW)
+                .addPermission("00000000-0000-0000-0000-000000000001", PERMISSION_ACTION__DELETE_ENTITY, PERMISSION_ALLOW)
+                .addPermission("00000000-0000-0000-0000-000000000001", PERMISSION_ACTION__READ_ENTITY, PERMISSION_ALLOW)
+                .addRole("00000000-0000-0000-0000-00000000000c", "00000000-0000-0000-0000-000000000008");
+
+        AAAEntityOperationsBuilder operationsBuilder = new AAAEntityOperationsBuilder();
+        operationsBuilder.add(createuserOperation);
+
+        return execute(serviceContext, operationsBuilder);
+    }
+
+    public static BaseResponse createComplexEntityForDevice(ServiceContext serviceContext, String ownerUuid, String name, String description, String apiKey) throws BException {
+
+        AAAEntityOperationBuilder createuserOperation = new AAAEntityOperationBuilder()
+                .setAction("1", ADD_OPE, "name", "description", "00000000-0000-0000-0000-000000000015", ownerUuid)
+                .setAuthWithApiKey(apiKey)
+                .addPermission("00000000-0000-0000-0000-000000000001", PERMISSION_ACTION__CREATE_ENTITY, PERMISSION_ALLOW)
+                .addPermission("00000000-0000-0000-0000-000000000001", PERMISSION_ACTION__DELETE_ENTITY, PERMISSION_ALLOW)
+                .addPermission("00000000-0000-0000-0000-000000000001", PERMISSION_ACTION__READ_ENTITY, PERMISSION_ALLOW)
+                .addRole("00000000-0000-0000-0000-000000000015", "00000000-0000-0000-0000-000000000007");
+
+        AAAEntityOperationsBuilder operationsBuilder = new AAAEntityOperationsBuilder();
+        operationsBuilder.add(createuserOperation);
+
+        return execute(serviceContext, operationsBuilder);
+    }
+
+    private static BaseResponse execute(ServiceContext serviceContext, AAAEntityOperationsBuilder operationsBuilder) throws BException {
+        IAAAClient aaaClient = serviceContext.getAaaClient();
+
+        BaseRequest registerUserRequest = new BaseRequest();
+
+        try {
+            registerUserRequest.getPayload().put(operationsBuilder.getObjectName(),operationsBuilder.build());
+        } catch(BJsonException e) {
+            throw new BException(e);
+        }
+
+        RequestInfo requestInfo = new RequestInfo(registerUserRequest, serviceContext.getServiceApiKey(), serviceContext.getServiceUuidAsString(), "", serviceContext.getAaaTransport(), null);
+        requestInfo.setSyncResponse();
+
+        return  aaaClient.doCreateComplexEntity(requestInfo, serviceContext.getServiceApiKey());
+    }
+
+}
